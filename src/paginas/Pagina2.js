c54fc2f5c4957e5621d1c7337a30677d95c3e3f6
jarboles <jarboles@contratista.epm.co>
2019-12-06 13:50:38 -0500

    Se agrega el Axios para peticion

--- src/paginas/Pagina2.js
+++ src/paginas/Pagina2.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import {Card} from '../componentes/Funcioonales'
 
 class Pagina2 extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            elementos:[]
+        }
+        this.obtener = this.obtener.bind(this);
+    }
+
+
+    async obtener() {
+        var respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        console.log(respuesta.data)
+        this.setState({elementos:respuesta.data.results})
+    }
+
+    pintar() {
+        if(this.state.elementos.length > 0){
+            var res = this.state.elementos.map((el,i) => {
+                return <Card name = {el.name}/>
+            })
+            return res;
+        }
+    }
+
     render(){
         return (
             <div className="card-principal">
                 Pagina 2
+                <button className="btn btn-primary" onClick={this.obtener}>Click</button>
+                <div className="container">
+                    {this.pintar()}
+                </div>
             </div>
         )
     }
